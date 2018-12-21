a01c0bf82f29c5f4172e39e475e80f99d7db309d
Jamie Ly <jamie.ly@gmail.com>
2018-12-20 22:07:31 -0500

    Added homebrew formula to install the command

---
+++ homebrew_formula.rb
@@ -0,0 +1,23 @@
+# Use like:
+# brew install https://raw.githubusercontent.com/jamiely/Icomoon.swift/master/homebrew_formula.rb
+#
+class IcomoonSwift < Formula
+  desc "Use your Icomoon fonts with Swift - auto-generates type safe enums for each icon"
+  homepage "https://github.com/jamiely/Icomoon.swift"
+  url "https://github.com/jamiely/Icomoon.swift/archive/20181220.tar.gz"
+  sha256 "2e02d83d5de76d8938c6ec671abefebf5ec7e6ffae8c7275e27ea2e1e05e9fb3"
+
+  skip_clean "lib/icomoon-swift/Icomoon"
+
+  def install
+    mv "run.sh", "icomoon-swift"
+    bin.install "icomoon-swift"
+    chmod_R "u+rw", "Icomoon"
+    (lib/"icomoon-swift").install Dir["**"] - Dir["icomoon-swift"]
+    chmod_R "u+w", lib/"icomoon-swift/Icomoon"
+  end
+
+  test do
+    system "#{bin}/icomoon-swift"
+  end
+end
